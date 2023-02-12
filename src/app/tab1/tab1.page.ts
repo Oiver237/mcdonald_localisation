c138bd489aaab88f9133c9df6f7e4e4188c08f23
Sami <sami-la98@hotmail.fr>
2023-02-12 16:18:00 +0100

    fetching fork api and test

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import * as Leaflet from 'leaflet';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -10,7 +10,36 @@ import * as Leaflet from 'leaflet';
 export class Tab1Page implements OnInit, OnDestroy {
   map: any;
 
-  constructor() { }
+  constructor(public http: HttpClient) { 
+   	// API Call
+    restaurantData: {
+      name: '';
+      slug: '';
+      geo: {
+        lat: '';
+        lng: '';
+      };
+
+    }
+
+		let headers = new HttpHeaders({
+			'x-rapidapi-host': 'the-fork-the-spoon.p.rapidapi.com',
+			'x-rapidapi-key': '34f5317b57msh9070f3a81a2c21cp108d50jsn8513f7ab3382',
+
+
+		});
+    let options = { headers: headers };
+    this.readApi("/restaurants/v2/list?queryPlaceValueCityId=60566&pageSize=30&pageNumber=1", options).subscribe(data => {
+      console.log(data);
+
+    }
+    );
+    
+  }
+
+  readApi(Url: string, headers: any) {
+    return this.http.get('https://the-fork-the-spoon.p.rapidapi.com' + Url, headers);
+  }
 
   ngOnInit() { }
   
@@ -25,6 +54,10 @@ export class Tab1Page implements OnInit, OnDestroy {
     Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
     }).addTo(this.map);
+
+    // add marker for any restaurant using the api
+  
+    
   }
 
 
@@ -32,5 +65,8 @@ export class Tab1Page implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.map.remove();
   }
+
+  // fetch api fork and the spoon 
+
 
 }
